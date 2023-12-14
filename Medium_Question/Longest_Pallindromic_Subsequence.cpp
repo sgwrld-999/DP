bac379deb9e8a5a74eea0c5d17b2a87b5e6ff45e
sgwrld-999 <130132265+sgwrld-999@users.noreply.github.com>
2023-12-14 21:55:00 +0530

    LCS + Pallindromic

--- Medium_Question/Longest_Pallindromic_Subsequence.cpp
+++ Medium_Question/Longest_Pallindromic_Subsequence.cpp
@@ -52,3 +52,40 @@ int main(int argc, char const *argv[])
     cout<<endl;
     return 0;
 }
+/*
+Problem naem : Longest Pallindromic Subsequence
+Problem Link : https://leetcode.com/problems/longest-palindromic-subsequence/
+Java code of using LCS 
+_________________________________________________________________________________________
+class Solution {
+    public int solveUsingMemoization(String a, String b, int i, int j, int[][] dp) {
+        if (i == a.length() || j == b.length())
+            return 0;
+
+        if (dp[i][j] != -1) 
+            return dp[i][j];
+
+        if (a.charAt(i) == b.charAt(j))
+            return dp[i][j] = 1 + solveUsingMemoization(a, b, i + 1, j + 1, dp);
+        else
+            return dp[i][j] = Math.max(solveUsingMemoization(a, b, i, j + 1, dp), solveUsingMemoization(a, b, i + 1, j, dp));
+    }
+
+    public int longestPalindromeSubseq(String s) {
+        int n = s.length();
+        StringBuilder revStr = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            revStr.append(s.charAt(n - 1 - i));
+        }
+
+        int[][] dp = new int[n + 1][n + 1];
+
+        for (int[] row : dp) {
+            Arrays.fill(row, -1);
+        }
+
+        return solveUsingMemoization(s, revStr.toString(), 0, 0, dp); 
+    }
+}
+_________________________________________________________________________________________
+*/
